7af78885f2892525ece2bbfedc106994f12d5623
henrrius <henrrybrgt@gmail.com>
2024-06-03 17:54:34 -0400

    deleting chirps

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('chirps', \App\Http\Controllers\ChirpController::class)
-    ->only(['index', 'store', 'update'])
+    ->only(['index', 'store', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
